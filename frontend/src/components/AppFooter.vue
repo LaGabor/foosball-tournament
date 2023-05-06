a7ee9d0f2a7729209085fb1135f3e3b2f549606e
LaGabor <lagaborka@gmail.com>
2023-05-06 16:03:00 +0200

    feat: vue-router and bootstrap 5 added to the project, AppFooter.vue and AppNavbar.vue components created and used in App.vue

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer class="footer fixed-bottom">
+    <div class="container">
+      <ul class="nav justify-content-center border-bottom pb-2 mb-1">
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Teams</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Tournaments</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Games</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">Top Lists</a>
+        </li>
+      </ul>
+      <p class="text-center text-muted">2023 FoosballFiesta</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/AppNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">FoosballFiesta</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Teams</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Tournaments</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Games</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Toplist</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-light" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+
+<script>
+export default {
+  name: 'AppNavbar',
+
+}
+</script>
